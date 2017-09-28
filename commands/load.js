ada7d18ea4208d71c44db3887e436feb9636f906
Tobias Davis <tdavis@fnoex.com>
2017-09-28 11:34:29 -0500

    move amazon commands into separate file for readability

--- commands/load.js
+++ commands/load.js
@@ -1,119 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-const AWS = require('aws-sdk')
 const { waterfall, constant } = require('async')
 
-const SNS_TOPIC_NAME = 'imperial_lambda'
-const SQS_QUEUE_NAME= 'imperial_lambda'
+const {
+    createTopic,
+    createQueue,
+    getQueueArn,
+    subscribeToTopic,
+    setQueueAttributes,
+    createLambda
+} = require('./../lib/amazon-commands')
+
 const DEFAULT_REGION = 'us-west-2'
-
-const createTopic = sns => (context, cb) => {
-    sns.createTopic({
-        Name: SNS_TOPIC_NAME
-    }, (error, result) => {
-        if (error) {
-            cb(error, context)
-        } else {
-            context.TopicArn = result.TopicArn
-            cb(null, context)
-        }
-    })
-}
-
-const createQueue = sqs => (context, cb) => {
-    sqs.createQueue({
-        QueueName: SQS_QUEUE_NAME,
-        Attributes: {
-            VisibilityTimeout: '0',
-            MessageRetentionPeriod: '300'
-        }
-    }, (error, result) => {
-        if (error) {
-            cb(error, context)
-        } else {
-            context.QueueUrl = result.QueueUrl
-            cb(null, context)
-        }
-    })
-}
-
-const getQueueArn = sqs => (context, cb) => {
-    sqs.getQueueAttributes({
-        QueueUrl: context.QueueUrl,
-        AttributeNames: [ 'QueueArn' ]
-    }, (error, result) => {
-        if (error) {
-            cb(error, context)
-        } else {
-            context.QueueArn = result.Attributes.QueueArn
-            cb(null, context)
-        }
-    })
-}
-
-const subscribeToTopic = sns => (context, cb) => {
-    sns.subscribe({
-        TopicArn: context.TopicArn,
-        Protocol: 'sqs',
-        Endpoint: context.QueueArn
-    }, (error, result) => {
-        cb(error, context)
-    })
-}
-
-const setQueueAttributes = sqs => (context, cb) => {
-    const attributes = {
-        Version: '2008-10-17',
-        Id: `${context.QueueArn}/SQSDefaultPolicy`,
-        Statement: [{
-            Sid: `Sid${new Date().getTime()}`,
-            Effect: 'Allow',
-            Principal: {
-                AWS: '*'
-            },
-            Action: 'SQS:SendMessage',
-            Resource: context.QueueArn,
-            Condition: {
-                ArnEquals: {
-                    'aws:SourceArn': context.TopicArn
-                }
-            }
-        }]
-    }
-
-    sqs.setQueueAttributes({
-        QueueUrl: context.QueueUrl,
-        Attributes: {
-            Policy: JSON.stringify(attributes)
-        }
-    }, (error, result) => {
-        cb(error, context)
-    })
-}
-
-const createLambda = ({ buildFolder, role, lambda }) => new Promise((resolve, reject) => {
-    const parameters = {
-        Code: {
-            ZipFile: fs.readFileSync(path.join(buildFolder, 'build.zip'))
-        },
-        FunctionName: 'imperialLambda',
-        Handler: 'index.handler',
-        Role: role,
-        Runtime: 'nodejs6.10',
-        Description: 'Imperial Lambda: https://github.com/saibotsivad/imperial-lambda',
-        MemorySize: 512,
-        Publish: true,
-        Timeout: 300
-    }
-
-    lambda.createFunction(parameters, (error, data) => {
-        if (error) {
-            reject(error)
-        } else {
-            resolve(data)
-        }
-    })
-})
 
 module.exports = ({ cwd, buildFolder, script, role, lambdas, concurrent, data }) => {
     if (!process.env.AWS_REGION) {
